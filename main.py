831f9b694a23c2249f735801102ffc3dc804cd12
paulorec <paulorec@gmail.com>
2018-08-09 19:11:51 -0300

    project

---
+++ main.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Importe as bibliotecas necessárias para o projeto.
+import numpy as np
+import pandas as pd
+from time import time
+from IPython.display import display # Permite a utilização da função display() para DataFrames.
+from sklearn.preprocessing import MinMaxScaler
+
+# Carregando os dados do Censo
+data = pd.read_csv("census.csv")
+
+#display(data.head(n=10))
+
+n_records = data.size
+
+# Número de registros com remuneração anual superior à $50,000
+n_greater_50k = len(filter(lambda x : x == '>50K',data.income))
+
+# O número de registros com remuneração anual até $50,000
+n_at_most_50k = len(filter(lambda x : x == '<=50K' ,data.income))
+
+# O percentual de indivíduos com remuneração anual superior à $50,000
+greater_percent = (n_greater_50k*100) /n_records
+
+# Exibindo os resultados
+print "Total number of records: {}".format(n_records)
+print "Individuals making more than $50,000: {}".format(n_greater_50k)
+print "Individuals making at most $50,000: {}".format(n_at_most_50k)
+print "Percentage of individuals making more than $50,000: {:.2f}%".format(greater_percent)
+
+income_raw = data['income']
+features_raw = data.drop('income', axis = 1)
+
+# Aplicando a transformação de log nos registros distorcidos.
+skewed = ['capital-gain', 'capital-loss']
+features_log_transformed = pd.DataFrame(data = features_raw)
+features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x + 1))
+
+scaler = MinMaxScaler() # default=(0, 1)
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+features_log_minmax_transform = pd.DataFrame(data = features_log_transformed)
+features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+# Exibindo um exemplo de registro com a escala aplicada
+display(features_log_minmax_transform.head(n=5))
+features_final = pd.get_dummies(features_log_minmax_transform)
+
+# TODO: Faça o encode da coluna 'income_raw' para valores numéricos
+income = features_final.loc[income_raw == ">50K"]
+
+# Exiba o número de colunas depois do one-hot encoding
+encoded = list(features_final.columns)
+print "{} total features after one-hot encoding.".format(len(encoded))
+
+print encoded
